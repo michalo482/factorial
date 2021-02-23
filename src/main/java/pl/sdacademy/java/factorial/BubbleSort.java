269957acfb65f0d5c48883c6358b1e8b813dc7bd
Michal Ostrowski <dakkon@o2.pl>
2021-02-23 17:58:25 +0100

    Initial commit

---
+++ src/main/java/pl/sdacademy/java/factorial/BubbleSort.java
@@ -0,0 +1,16 @@
+package pl.sdacademy.java.factorial;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        int[] values = new int[] {5,7,1,3,9,0};
+
+    }
+
+    public static void sort(int[] values) {
+        for (int i = 0; i < values.length; i++ ) {
+            for (int j = 0; j < values.length; j++) {
+                if (values[j] <  )
+            }
+        }
+    }
+}
---
+++ src/main/java/pl/sdacademy/java/factorial/Main.java
@@ -0,0 +1,7 @@
+package pl.sdacademy.java.factorial;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
